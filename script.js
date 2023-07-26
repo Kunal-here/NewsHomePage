083d228c74b17c1bf0a69dcec7490602e6592ee2
Kunal Jadhav <127647392+Kunal-here@users.noreply.github.com>
2023-07-26 22:55:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+const hamburger = document.querySelector('#hamburger')
+const ul = document.querySelector('ul');
+const close = document.querySelector('#hamburger-close')
+
+
+
+
+hamburger.addEventListener("click", () => {
+  ul.classList.toggle('visible'); // Toggle the class to show/hide the slide bar
+
+  let ham_child = hamburger.firstElementChild;
+  if(ul.classList.contains('visible')){
+    ham_child.classList.replace('fa-bars', 'fa-xmark')
+  }
+  else {
+    ham_child.classList.replace('fa-xmark','fa-bars' )
+  }
+});
+
+// Add an event listener to detect when the transition ends and hide the slide bar
+ul.addEventListener("transitionend", () => {
+  if (!ul.classList.contains('visible')) {
+    ul.classList.add('hidden');
+  }
+
+  
+});
+
+
+
+
+
+
+
+
+
+
+
